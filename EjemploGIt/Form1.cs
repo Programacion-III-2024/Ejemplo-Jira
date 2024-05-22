6fa96f5dd37659ce20bf97f05ef333993377102b
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2024-05-21 21:52:00 -0300

    Agregamos saludo cuando inicia el formulario

--- EjemploGIt/Form1.cs
+++ EjemploGIt/Form1.cs
@@ -16,5 +16,10 @@ namespace EjemploGIt
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Holi");
+        }
     }
 }
